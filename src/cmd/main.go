f238419b9dbb3b281ad5ad856a325f3e5665bf28
Ali Elmi <ali.elmi77@yahoo.com>
2025-01-23 16:27:20 +0330

    feat: Add Swagger documentation
    - Integrated Swagger UI to provide interactive API documentation
    - Added Swagger annotations to controller methods to automatically generate API documentation
    - Configured Swagger to serve the documentation at the `/swagger` endpoint

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/alielmi98/Go-Blog-API/config"
+	"github.com/alielmi98/Go-Blog-API/docs"
 	"github.com/alielmi98/Go-Blog-API/routes"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -11,11 +16,26 @@ func main() {
 	r := gin.Default()
 
 	// Initialize the database connection
-	db, _ := config.ConnectDB()
+	db, err := config.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Register the routes
 	routes.RegisterRoutes(r, db)
-
+	RegisterSwagger(r)
 	// Start the server
 	r.Run(":8080")
+
+}
+
+func RegisterSwagger(r *gin.Engine) {
+	docs.SwaggerInfo.Title = "golang web api"
+	docs.SwaggerInfo.Description = "golang web api"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.BasePath = "/api"
+	docs.SwaggerInfo.Host = "localhost:8080"
+	docs.SwaggerInfo.Schemes = []string{"http"}
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
--- src/controllers/post_controller.go
+++ src/controllers/post_controller.go
@@ -17,80 +17,128 @@ func NewPostController(postService services.PostService) *PostController {
 	return &PostController{postService: postService}
 }
 
+// CreatePost godoc
+// @Summary Create a new post
+// @Description Create a new blog post with the provided details
+// @Tags posts
+// @Accept json
+// @Produce json
+// @Param post body dto.CreatePostDTO true "Post data"
+// @Success 201 {object} models.Post
+// @Failure 400 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /posts [post]
 func (pc *PostController) CreatePost(c *gin.Context) {
 	var postDTO dto.CreatePostDTO
 	if err := c.ShouldBindJSON(&postDTO); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, map[string]interface{}{"error": err.Error()})
 		return
 	}
 
 	post, err := pc.postService.CreatePost(c, &postDTO)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
 		return
 	}
 
 	c.JSON(http.StatusCreated, post)
 }
 
+// UpdatePost godoc
+// @Summary Update an existing post
+// @Description Update a post by ID with the provided details
+// @Tags posts
+// @Accept json
+// @Produce json
+// @Param id path string true "Post ID"
+// @Param post body dto.UpdatePostDTO true "Updated Post data"
+// @Success 200 {object} models.Post
+// @Failure 400 {object} map[string]interface{}
+// @Failure 404 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /posts/{id} [put]
 func (pc *PostController) UpdatePost(c *gin.Context) {
 	var updatePostDTO dto.UpdatePostDTO
 	id := c.Params.ByName("id")
 
 	if err := c.ShouldBindJSON(&updatePostDTO); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, map[string]interface{}{"error": err.Error()})
 		return
 	}
 
 	_, err := pc.postService.GetPostByID(c, id)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Post not found"})
+		c.JSON(http.StatusNotFound, map[string]interface{}{"error": err.Error()})
 		return
 	}
 
 	post, err := pc.postService.UpdatePost(c, id, &updatePostDTO)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
 		return
 	}
 
 	c.JSON(http.StatusOK, post)
 }
 
+// DeletePost godoc
+// @Summary Delete a post
+// @Description Delete a post by ID
+// @Tags posts
+// @Param id path string true "Post ID"
+// @Success 200 {string} string "Deleted post successfully"
+// @Failure 404 {object} map[string]interface{}
+// @Failure 500 {object} map[string]interface{}
+// @Router /posts/{id} [delete]
 func (pc *PostController) DeletePost(c *gin.Context) {
 	id := c.Params.ByName("id")
 
 	_, err := pc.postService.GetPostByID(c, id)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Post not found"})
+		c.JSON(http.StatusNotFound, map[string]interface{}{"error": "post not found"})
 		return
 	}
 
 	err = pc.postService.DeletePost(c, id)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
 		return
 	}
 
 	c.JSON(http.StatusOK, "Deleted post successfully")
 }
 
+// GetPostById godoc
+// @Summary Get a post by ID
+// @Description Retrieve a post by its ID
+// @Tags posts
+// @Param id path string true "Post ID"
+// @Success 200 {object} models.Post
+// @Failure 404 {object} map[string]interface{}
+// @Router /posts/{id} [get]
 func (pc *PostController) GetPostById(c *gin.Context) {
 	id := c.Params.ByName("id")
 
 	post, err := pc.postService.GetPostByID(c, id)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Post not found"})
+		c.JSON(http.StatusNotFound, map[string]interface{}{"error": "post not found"})
 		return
 	}
 
 	c.JSON(http.StatusOK, post)
 }
 
+// GetAllPosts godoc
+// @Summary Get all posts
+// @Description Retrieve a list of all posts
+// @Tags posts
+// @Success 200 {array} models.Post
+// @Failure 500 {object} map[string]interface{}
+// @Router /posts [get]
 func (pc *PostController) GetAllPosts(c *gin.Context) {
 	posts, err := pc.postService.GetAllPosts(c)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, posts)
